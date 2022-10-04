6dff0fd55efc2a2a3ef89c1d56ddf85b7bfee39c
joanroamora <41650757+joanroamora@users.noreply.github.com>
2022-10-03 22:03:22 -0500

    HOLA MUNDO

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-apt update
-apt install -y nodejs
-apt install -y npm
-nodejs --version
-npm --version
+#apt update
+#apt install -y nodejs
+#apt install -y npm
+#nodejs --version
+#npm --version
+echo "HOLA MUNDO"
